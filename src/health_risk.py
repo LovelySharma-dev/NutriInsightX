8bda715270019096a6d35eb36fda9d223d82a8ac
Lovely <lovelysharmacji@gmail.com>
2026-06-10 02:07:10 +0530

    Added Risk Scoring

--- src/health_risk.py
+++ src/health_risk.py
@@ -30,28 +30,75 @@ class HealthRiskScorer:
         except:
             energy = 0
 
-        if sugar > 22.5:
-            score += 25
+        # Sugar Risk
 
-        if salt > 1.5:
-            score += 25
+        
+        if sugar <= 5:
+            sugar_points = 0
+        elif sugar <= 15:
+            sugar_points = 10
+        elif sugar <= 22.5:
+            sugar_points = 20
+        else:
+            sugar_points = 25
 
-        if saturated_fat > 5:
-            score += 25
+        score += sugar_points
 
-        if energy > 400:
-            score += 25
+        # Salt Risk
 
-        if score <= 25:
+        if salt <= 0.3:
+            salt_points = 0
+        elif salt <= 1.0:
+            salt_points = 10
+        elif salt <= 1.5:
+            salt_points = 20
+        else:
+            salt_points = 25
+
+        score += salt_points
+
+        # Saturated Fat Risk
+
+        if saturated_fat <= 1.5:
+            fat_points = 0
+        elif saturated_fat <= 3:
+            fat_points = 10
+        elif saturated_fat <= 5:
+            fat_points = 20
+        else:
+            fat_points = 25
+
+        score += fat_points
+
+        # Energy Risk
+
+        if energy <= 100:
+            energy_points = 0
+        elif energy <= 250:
+            energy_points = 10
+        elif energy <= 400:
+            energy_points = 20
+        else:
+            energy_points = 25
+
+        score += energy_points
+        # Final Risk Level
+        if score <= 20:
             level = "Low Risk"
-        elif score <= 50:
+        elif score <= 40:
             level = "Moderate Risk"
-        elif score <= 75:
+        elif score <= 70:
             level = "High Risk"
         else:
             level = "Very High Risk"
 
         return {
-            "score": score,
-            "risk_level": level
-        }
+    "score": score,
+    "risk_level": level,
+    "factors": {
+        "Sugar": sugar_points,
+        "Salt": salt_points,
+        "Saturated Fat": fat_points,
+        "Energy": energy_points
+    }
+}
